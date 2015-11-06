e4e6932b80071c09f778e6e219086cfc0bc2109f
Yumen Tsao <gawainus97@gmail.com>
2015-11-06 10:10:00 -0600

    Fixed wrong names

--- ClangDeauto.cpp
+++ ClangDeauto.cpp
@@ -32,7 +32,7 @@ using clang::tooling::newFrontendActionFactory;
 
 // Apply a custom category to all command-line options so that they are the
 // only ones displayed.
-static llvm::cl::OptionCategory MyToolCategory("my-tool options");
+static llvm::cl::OptionCategory ClangDeautoCategory("clang-deauto options");
 
 // CommonOptionsParser declares HelpMessage with a description of the common
 // command-line options related to the compilation database and input files.
@@ -40,10 +40,10 @@ static llvm::cl::OptionCategory MyToolCategory("my-tool options");
 static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
 
 // A help message for this specific tool can be added afterwards.
-static cl::extrahelp MoreHelp("\nMore help text...");
+static cl::extrahelp MoreHelp("\nContact Yumen for more help.");
 
 int main(int argc, const char **argv) {
-  CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
+  CommonOptionsParser OptionsParser(argc, argv, ClangDeautoCategory);
   ClangTool Tool(OptionsParser.getCompilations(),
                  OptionsParser.getSourcePathList());
   return Tool.run(newFrontendActionFactory<clang::SyntaxOnlyAction>().get());
